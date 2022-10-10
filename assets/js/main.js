a2854eed14dc0b4838a45c91984b402c26ee87d5
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-10 14:11:15 +0200

    initial commit: setup and comments

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+/* Scrivi un programma che stampi in console i numeri da 1 a 100:
+    1- per i multipli di 3 stampa “Fizz” al posto del numero
+    2- per i multipli di 5 stampa “Buzz”.
+    3- Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
+*/
+
+/* Passaggi:
+    1- creare un for loop (da 1 a 100)
+    2- if statement con 3 condizioni:
+        2.1 Se il n. è divisibile per 3 == "Fizz"
+        2.2 Se il n. è divisibile per 5 == "Buzz"
+        2.3 Se il n. è divisibile per 3 e per 5 == "Fizzbuzz"
+    3- else: mi stampi il n. 
+*/
+/* Strumenti */
+
+// for loop: per stampare i numeri da 1 a 100
+// if statement: per determinare in base a quali condizioni deve mostrarmi a schermo solo il numero o "Fizz" (se è divisibile per 3) o "Buzz" (se è divisibile per 5) o "Fizzbuzz" (se è divisbile per entrambi)
+// % : per determinare se il numero sia divisibile per 3, 5 o per entrambi
